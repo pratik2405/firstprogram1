f4e8204c361692988c0723fd4115431667eaf38d
pratik2405 <shinde7388pratik@gmail.com>
2024-11-13 15:58:51 +0530

    Add project files.

---
+++ firstprogram1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstprogram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hiiii");
+
+            int year = 2023;
+            string month = "oct";
+
+            //concate
+
+            Console.WriteLine("month=" + month + " Year=" + year);
+
+            //using placeholder{}
+
+            Console.WriteLine("month= {0} year={1}",month,year);
+
+
+            //Check year is leep or not
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("This is leep year");
+            }
+            else {
+                Console.WriteLine("This is not leep year");
+                  }
+
+            //positive or negative
+
+            int num = -5;
+
+            if (num > 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if (num < 0) {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is negative");
+            }
+
+
+            //odd or even
+
+            int num2 = 5;
+
+            if (num2 % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+
+            }
+            else { 
+            Console.WriteLine("Number id odd");
+            }
+
+            //user input
+            Console.WriteLine("Enter number=");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ firstprogram1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("firstprogram1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("firstprogram1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73558200-9f64-4251-93f8-ba032a5b9174")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
